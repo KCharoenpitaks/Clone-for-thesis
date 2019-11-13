f36760f56706af0546a638e345ec6c26ac3da46a
KCharoenpitaks <50170318+KCharoenpitaks@users.noreply.github.com>
2019-11-13 07:41:08 +0700

    Update test_run.sh

--- update/test_run.sh
+++ update/test_run.sh
@@ -6,10 +6,17 @@
 
 
 python3 src/main.py --config=coma_smac with running_mode=4 --env-config=sc2
+pkill -t pts/1
 python3 src/main.py --config=coma_smac with running_mode=3 --env-config=sc2
+pkill -t pts/1
 python3 src/main.py --config=coma_smac with running_mode=2 --env-config=sc2
+pkill -t pts/1
 python3 src/main.py --config=coma_smac with running_mode=1 --env-config=sc2
+pkill -t pts/1
 python3 src/main.py --config=coma_smac with running_mode=0 --env-config=sc2
+pkill -t pts/1
 python3 src/main.py --config=coma_smac with running_mode=normal --env-config=sc2
+pkill -t pts/1
+
 
 
